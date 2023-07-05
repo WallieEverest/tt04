20e18cc50c062201fe32680fdfb97be880ed2b78
Wallie Everest <wallie.everest@gmail.com>
2023-07-04 21:40:07 -0400

    Stub with clk and oe

--- src/tt_um_morningjava_top.v
+++ src/tt_um_morningjava_top.v
@@ -11,11 +11,18 @@
 `default_nettype none
 
 module tt_um_morningjava_top (
-  input  wire [7:0] io_in,
-  output wire [7:0] io_out,
-  inout  wire [7:0] io_bidir
+  input  wire       clk,      // clock
+  input  wire       rst_n,    // reset_n - low to reset
+  input  wire       ena,      // will go high when the design is enabled
+  input  wire [7:0] ui_in,    // Dedicated inputs
+  input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+  output wire [7:0] uo_out,   // Dedicated outputs
+  output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+  output wire [7:0] uio_oe    // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+
 );
-  assign io_out = io_in;
-  assign io_bidir[7:4] = io_bidir[3:0];
+  assign uo_out = clk ? ui_in : 8'x0;
+  assign uio_out[7:4] = rst_n ? uio_in[3:0] : 4'x0;
+  assign uio_oe = ena ? 8'xF0 : 8'x00;
   
 endmodule
